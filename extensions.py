6c92fc97b96d0f6b6f620bd60ed0cc018d272fa0
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-03-27 16:17:51 +0100

    docs(extensions): added explanation for SQLAlchemy instance setup

--- extensions.py
+++ extensions.py
@@ -1,3 +1,5 @@
+# Initialize SQLAlchemy instance for use across the app
 from flask_sqlalchemy import SQLAlchemy
 
+# This `db` object will be imported and used in models and app factory
 db = SQLAlchemy()
